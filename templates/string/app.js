fdaaa3df941efa33fe187746d3ee88faccc7acb2
kkomaz <leeajl006@gmail.com>
2015-07-12 21:21:43 -0400

    modieifed for inline templating

--- templates/string/app.js
+++ templates/string/app.js
@@ -13,6 +13,14 @@
   })
   .controller('TestCtrl', TestCtrl);
 
+  angular.module('app').run(function ($templateCache){
+    $templateCache.put('test.html');
+  });
+
 })();
 
 
+// Since $templateCache is a service, 
+// we won't be able to access it in the config phase of our app. 
+// The earliest we can access $templateCache is in the run phase for our app. 
+// Angular modules can have as many run blocks as needed.
